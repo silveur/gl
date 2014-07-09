29f9943b1c10b960f7cfd05551d42948d9608834
silveur <silvere.letellier@gmail.com>
2014-07-09 16:01:28 +0100

    Initial Commit

---
+++ glTests/glTests.m
@@ -0,0 +1,34 @@
+//
+//  glTests.m
+//  glTests
+//
+//  Created by silvere letellier on 09/07/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface glTests : XCTestCase
+
+@end
+
+@implementation glTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

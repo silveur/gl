db7ca13dee590660147e6868b78512c4d187086c
silveur <silvere.letellier@gmail.com>
2014-07-11 11:59:50 +0100

    image drawn on rectangle

--- gl/ViewController.m
+++ gl/ViewController.m
@@ -11,17 +11,20 @@
 typedef struct
 {
 	GLKVector3 positionCoordinates;
+	GLKVector2 textureCoordinates;
 	
 } VertexData;
 
+#define SQUARE_SIZE	80
+
 VertexData vertices[] =
 {
-	{ -0.5f,-0.5f, 0.0f },
-	{ 0.5f, -0.5f, 0.0f },
-	{ -0.5f, 0.5f, 0.0f },
-	{ -0.5f, 0.5f, 0.0f },
-	{ 0.5f, -0.5f, 0.0f },
-	{ 0.5f, 0.5f, 0.0f  }
+	{{ 0.0f, 0.0f, 0.0f }, {0.0f, 0.0f}},
+	{{ SQUARE_SIZE, 0.0f, 0.0f }, {1.0f, 0.0f}},
+	{{ 0.0f, SQUARE_SIZE, 0.0f }, {0.0f, 1.0f}},
+	{{ 0.0f, SQUARE_SIZE, 0.0f }, {0.0f, 1.0f}},
+	{{ SQUARE_SIZE, 0.0f, 0.0f }, {1.0f, 0.0f}},
+	{{ SQUARE_SIZE, SQUARE_SIZE, 0.0f  }, {1.0f, 1.0f}}
 };
 
 @interface ViewController ()
@@ -43,16 +46,32 @@ VertexData vertices[] =
 	[EAGLContext setCurrentContext:self.context];
 	
 	self.baseEffect = [[GLKBaseEffect alloc] init];
-	self.baseEffect.useConstantColor = YES;
-	self.baseEffect.constantColor = GLKVector4Make(1.0f, 0.0f, 0.0f, 1.0f);
+//	self.baseEffect.useConstantColor = YES;
+//	self.baseEffect.constantColor = GLKVector4Make(1.0f, 0.0f, 0.0f, 1.0f);
 	glClearColor(1.0f, 1.0f, 1.0f, 0.5f);
+	
+	self.baseEffect.transform.projectionMatrix = GLKMatrix4MakeOrtho(0, 640, 0, 1136, 0, 0);
 	
 	glGenBuffers(1, &_vertexBufferID);
 	glBindBuffer(GL_ARRAY_BUFFER, _vertexBufferID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
 	glEnableVertexAttribArray(GLKVertexAttribPosition);
-	glVertexAttribPointer(GLKVertexAttribPosition,3, GL_FLOAT, GL_FALSE,sizeof(VertexData),NULL);
+	glVertexAttribPointer(GLKVertexAttribPosition,3, GL_FLOAT, GL_FALSE,sizeof(VertexData),offsetof(VertexData, positionCoordinates));
+	
+	
+	
+	CGImageRef imageReference = [[UIImage imageNamed:@"IconSmall.png"] CGImage];
+	
+	GLKTextureInfo* textureInfo = [GLKTextureLoader textureWithCGImage:imageReference options:[NSDictionary dictionaryWithObject:[NSNumber numberWithBool:YES] forKey:GLKTextureLoaderOriginBottomLeft] error:NULL];
+	self.baseEffect.texture2d0.name = textureInfo.name;
+	self.baseEffect.texture2d0.target = textureInfo.target;
+	
+	glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+	glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (GLvoid*)offsetof(VertexData, textureCoordinates));
+	
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 - (void) glkView:(GLKView *)view drawInRect:(CGRect)rect
@@ -66,7 +85,6 @@ VertexData vertices[] =
 - (void) update
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	NSLog(@"HELLO");
 }
 
 - (void)didReceiveMemoryWarning
